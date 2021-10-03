2bd5ef6627f191768e57c47a80bc0703a09ac2da
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2021-10-03 15:40:26 -0400

    bhjk

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,7 +3,7 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("header",{duration:2,alpha:0});
+mainTL.from("body >div p",{duration:2,alpha:0});
 
 // mainTL.from("#header",{duration:2, alpha:0, y:1000 })
 //     .from ("#header h1",{duration:3, alpha:0, y:"+=200"})
