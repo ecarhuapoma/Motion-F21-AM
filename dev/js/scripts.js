cc1582fc0abd55ff945d8b3ed46362a1d17906e5
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2021-10-03 16:40:29 -0400

    fix

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,18 +3,18 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("header ul #open",{duration:.1,alpha:0});
-mainTL.from("header ul #open h1",{duration:.3,alpha:0});
-mainTL.from("header ul #wiggy, #cream",{duration:.3,alpha:0,scale:.5});
-mainTL.from("header ul #cone",{duration:.2,alpha:0,scale:.5});
+mainTL.from("header ul #open",{duration:.1,alpha:0})
+.from("header ul #open h1",{duration:.3,alpha:0})
+.from("header ul #wiggy, #cream",{duration:.3,alpha:0,scale:.5})
+.from("header ul #cone",{duration:.2,alpha:0,scale:.5})
 
 // mainTL.from("header ul #cone",{duration:.2,alpha:0,scale:.5});
 // mainTL.from("header ul #wiggy",{duration:.5,alpha:0,scale:1.2});
 // mainTL.from("header open ",{duration:.5,alpha:0});
 // mainTL.from("header open",{duration:2,alpha:0,scale:2});
 
-mainTL.from("section ul #directions, #menu, #about, #events",{duration:.5,alpha:0});
-mainTL.from("#body",{duration:.5,alpha:0})
+.from("section ul #directions, #menu, #about, #events",{duration:.5,alpha:0})
+.from("#body",{duration:.5,alpha:0});
 
 // mainTL.from("#header",{duration:2, alpha:0, y:1000 })
 //     .from ("#header h1",{duration:3, alpha:0, y:"+=200"})
