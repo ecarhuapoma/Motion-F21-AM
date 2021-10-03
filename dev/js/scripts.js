cb8635fe06019b857a3c8257a3545577d7a28c74
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2021-10-03 15:26:08 -0400

    animation starting
    doesnt work yet

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,10 @@
 // import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+const mainTL = gsap.timeline();
+
+mainTL.from("header",{duration:2,alpha:0});
+
+// mainTL.from("#header",{duration:2, alpha:0, y:1000 })
+//     .from ("#header h1",{duration:3, alpha:0, y:"+=200"})
+//     .from ("#header h2",{duration:4, alpha:0, y:"+=200"});
