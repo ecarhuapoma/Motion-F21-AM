17adc0ccd2be69e3acfa7fae0c622fa5ecec2ba7
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2021-10-06 09:04:14 -0400

    finished
    YUP

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,6 +8,7 @@ mainTL.from("header ul #open",{duration:.1,alpha:0})
 .from("header ul #wiggy, #cream",{duration:.3,alpha:0,scale:.5})
 .from("header ul #cone",{duration:.2,alpha:0,scale:.5})
 
+
 // mainTL.from("header ul #cone",{duration:.2,alpha:0,scale:.5});
 // mainTL.from("header ul #wiggy",{duration:.5,alpha:0,scale:1.2});
 // mainTL.from("header open ",{duration:.5,alpha:0});
@@ -16,6 +17,14 @@ mainTL.from("header ul #open",{duration:.1,alpha:0})
 .from("section ul #directions, #menu, #about, #events",{duration:.5,alpha:0})
 .from("#body",{duration:.5,alpha:0});
 
-// mainTL.from("#header",{duration:2, alpha:0, y:1000 })
-//     .from ("#header h1",{duration:3, alpha:0, y:"+=200"})
-//     .from ("#header h2",{duration:4, alpha:0, y:"+=200"});
+//mainTL.from("#open",60,{duration:-1,backgroundPosition: "-2247px 0px"});
+
+const storeTL = gsap.timeline({repeat:-1});
+
+storeTL.from("#open",{duration:120,backgroundPosition: "-2247px 0px", ease:"none"});
+
+
+// sun animation broken
+
+// const backTL = gsap.timeline({repeat:-1});
+// backTL.from("base #body", 120,{duration:10,backgroundPosition: "-2247px 0px"});
