1c5251dde7ed32ef9ca0ba95bea27d2fe8343c5e
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2021-10-23 23:23:22 -0400

    worked further
    yup

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,34 +1,24 @@
 // import { gsap } from "gsap";
 import { gsap } from "gsap";
 
+//instantiates the timelines
 const preloadTL = gsap.timeline();
 const mainTL = gsap.timeline();
 
+//preloader appears
 preloadTL.from("#preloader div",{duration:.4,alpha:0,scale:.5})
 
-
+//header appears
 mainTL.from("header ul #open",{duration:.1,alpha:0})
 .from("header ul #open h1",{duration:.3,alpha:0})
 .from("header ul #wiggy, #cream",{duration:.3,alpha:0,scale:.5})
 .from("header ul #cone",{duration:.2,alpha:0,scale:.5})
 
-
-// mainTL.from("header ul #cone",{duration:.2,alpha:0,scale:.5});
-// mainTL.from("header ul #wiggy",{duration:.5,alpha:0,scale:1.2});
-// mainTL.from("header open ",{duration:.5,alpha:0});
-// mainTL.from("header open",{duration:2,alpha:0,scale:2});
-
+//buttons appear
 .from("section ul #directions, #menu, #about, #events",{duration:.5,alpha:0})
 .from("#body",{duration:.5,alpha:0});
 
-//mainTL.from("#open",60,{duration:-1,backgroundPosition: "-2247px 0px"});
-
+//rotating image
 const storeTL = gsap.timeline({repeat:-1});
-
 storeTL.from("#open",{duration:120,backgroundPosition: "-2247px 0px", ease:"none"});
 
-
-// sun animation broken
-
-// const backTL = gsap.timeline({repeat:-1});
-// backTL.from("base #body", 120,{duration:10,backgroundPosition: "-2247px 0px"});
