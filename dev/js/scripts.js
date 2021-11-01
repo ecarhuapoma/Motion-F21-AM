a073308c94d9a34744ef7fd932199553563649da
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2021-10-31 21:21:22 -0400

    worked on preloader animation
    need to iron out some problems

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,29 +1,69 @@
 // import { gsap } from "gsap";
+// import { gsap } from "gsap";
+// import { GSDevTools } from "gsap/GSDevTools";
+// import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+// import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+// gsap.registerPlugin(GSDevTools,DrawSVGPlugin, MotionPathPlugin);
+
 import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
-gsap.registerPlugin(GSDevTools);
+import { ExpoScaleEase, RoughEase, SlowMo } from "gsap/EasePack";
+import { EaselPlugin } from "gsap/EaselPlugin";
 
-// //instantiates the timelines
-// const preloadTL = gsap.timeline();
+gsap.registerPlugin(GSDevTools, EaselPlugin, ExpoScaleEase, RoughEase, SlowMo);
+
 const mainTL = gsap.timeline();
 
-function coneCreate(){
-    const tl = gsap.timeline();
-    tl.to("#cone1",{duration:.5, y:"-=200"});
-    return tl;
+//how do i center all the cone shapes so that they all line up better?
+
+gsap.set("preloader #preloaderanimation", {x:0, y:0});
+
+function icecream(){
+  const tl=gsap.timeline();
+  tl.to("#cone1", {duration:.1, autoAlpha: 1});
+  tl.to("#cone2", {duration:.1, autoAlpha: 1});
+  
+  tl.to("#cone1", {duration:.2, autoAlpha: 0});
+  tl.to("#cone3", {duration:.1, autoAlpha: 1});
+  
+  
+  tl.to("#cone3", {duration:.1, autoAlpha: 1});
+  tl.to("#cone2", {duration:.2, autoAlpha: 0});
+  
+  tl.to("#cone4", {duration:.1, autoAlpha: 1});
+  tl.to("#cone3", {duration:.2, autoAlpha: 0});
+  
+  tl.to("#cone5", {duration:.1, autoAlpha: 1});
+  tl.to("#cone4", {duration:.2, autoAlpha: 0});
+  
+  tl.to("#cone6", {duration:.1, autoAlpha: 1});
+  tl.to("#cone5", {duration:.2, autoAlpha: 0});
+
+  tl.to("#cone7", {duration:.1, autoAlpha: 1});
+  tl.to("#cone6", {duration:.2, autoAlpha: 0});
+
+  tl.to("#cone8", {duration:.1, autoAlpha: 1});
+  tl.to("#cone7", {duration:.2, autoAlpha: 0});
+
+  tl.to("#cone9", {duration:.1, autoAlpha: 1});
+  tl.to("#cone8", {duration:.2, autoAlpha: 0});
+  
+  tl.to("#cone10", {duration:.1, autoAlpha: 1});
+  tl.to("#cone9", {duration:.2, autoAlpha: 0});
+
+  return tl;
 }
 
-function coneCreate2(){
-    const tl = gsap.timeline();
-    tl.to("#cone2",{duration:.5, y:"-=200"});
-    return tl;
+function vanilla(){
+  const tl=gsap.timeline();
+  tl.to("#vanilla3", {duration:.5, autoAlpha:1});
+  return tl;
 }
 
-mainTL.add(coneCreate())
-        .add(coneCreate2());
+mainTL.add(icecream)
+.add(vanilla);
 
-// //preloader appears
-// preloadTL.from("#preloader div",{duration:.4,alpha:0,scale:.5})
+
 
 // //header appears
 // mainTL.from("header ul #open",{duration:.1,alpha:0})
